023e4303e6dc9a9ad5a5c2d82f989cc983ba9ba6
jpkim921 <jpkim921@gmail.com>
2017-10-17 01:08:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  return a / b
+  return a /= b
 }
 
 function inc(n) {
@@ -23,8 +23,9 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  var a = parseInt(n)
-
-  if (a )
+  return parseInt(n, 10)
 }
 
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
